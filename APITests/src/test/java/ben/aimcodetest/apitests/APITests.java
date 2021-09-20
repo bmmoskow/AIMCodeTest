70d7188824a9311c800e4b65a91e779fdafdb3c5
Ben Moskowitz <bmmoskow@amath.washington.edu>
2021-09-20 10:42:16 -0700

    improved documentation; remove unneeded references; minor test updates

--- APITests/src/test/java/ben/aimcodetest/apitests/APITests.java
+++ APITests/src/test/java/ben/aimcodetest/apitests/APITests.java
@@ -8,9 +8,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import static io.restassured.RestAssured.*;
 import io.restassured.response.*;
 import io.restassured.specification.*;
-import java.time.Instant;
-import java.util.UUID;
-import org.testng.Assert;
+import static java.time.Instant.now;
+import static java.util.UUID.randomUUID;
+import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 
 public class APITests {
@@ -22,35 +22,53 @@ public class APITests {
         httpRequest = given();
     }
 
+    /**
+     * test GET API and then the first 10 skus
+     * @throws JsonProcessingException
+     */
     @Test()
     public void getTest() throws JsonProcessingException {
         Item[] items = executeGetAllTest();
 
-        for (Integer i = 0; i < 2; i++) {
+        for (Integer i = 0; i < 10; i++) {
             executeGetTest(items[i]);
         }
     }
 
+    /**
+     * test the following
+     * 1. POST
+     * 2. GET for the just created sku
+     * 3. POST for the same sku to update the data
+     * 4. GET for the same sku to validate the updated data
+     * 5. DELETE the sku
+     * 6. GET to validate that the sku is gone
+     * @throws JsonProcessingException
+     */
     @Test()
     public void postGetDeleteTest() throws JsonProcessingException {
         ItemBase postItem = new ItemBase();
         postItem.description = "post test";
         postItem.price = "1.23";
-        postItem.sku = UUID.randomUUID().toString();
+        postItem.sku = randomUUID().toString();
 
+        Long minCreatedAt = now().getEpochSecond(); // time right before post
         Item postResponseItem = executePostTest(postItem);
+        Long maxCreatedAt = now().getEpochSecond(); // time right after post
 
         try {
-            executeGetTestWithTimeChecks(postResponseItem);
+            executeGetTestWithTimeChecks(postResponseItem, minCreatedAt, maxCreatedAt);
 
             ItemBase updatedPostItem = new ItemBase();
-
             updatedPostItem.description = postItem.description;
             postItem.price = "1.24";
             updatedPostItem.sku = postItem.sku;
 
+            Long minUpdatedAt = now().getEpochSecond(); // time right before post
             Item updatedPostResponseItem = executePostTest(updatedPostItem);
-            executeGetTestWithTimeChecks(updatedPostResponseItem);
+            Long maxUpdatedAt = now().getEpochSecond(); // time right before post
+
+            executeGetTestWithTimeChecks(updatedPostResponseItem, minCreatedAt, maxCreatedAt, minUpdatedAt, maxUpdatedAt);
         }
         finally {
             executeDeleteTest(postItem);
@@ -58,38 +76,56 @@ public class APITests {
         }
     }
 
+    /**
+     * test GET for an unknown sku
+     */
     @Test
     public void getUnknownItemTest() {
-        executeGetUnknownItemTest(UUID.randomUUID().toString());
+        executeGetUnknownItemTest(randomUUID().toString());
     }
 
+    /**
+     * test DELETE for an unknown sku
+     */
     @Test
     public void deleteUnknownItemTest() {
-        Response deleteResponse = httpRequest.delete("/" + UUID.randomUUID().toString());
+        Response deleteResponse = httpRequest.delete("/" + randomUUID().toString());
         int statusCode = deleteResponse.getStatusCode();
-        Assert.assertEquals(statusCode, 404, "should get 404 if the item to be deleted doesn't exist");
+        assertEquals(statusCode, 404, "should get 404 if the item to be deleted doesn't exist");
     }
 
+    /**
+     * test POST for an empty description
+     * @throws JsonProcessingException
+     */
     @Test()
     public void postEmptyDescriptionTest() throws JsonProcessingException {
         ItemBase postItem = new ItemBase();
         postItem.description = "";
         postItem.price = "1.23";
-        postItem.sku = UUID.randomUUID().toString();
+        postItem.sku = randomUUID().toString();
 
         executeNegativePostTest(postItem);
     }
 
+    /**
+     * test POST for an empty price
+     * @throws JsonProcessingException
+     */
     @Test()
     public void postEmptyPriceTest() throws JsonProcessingException {
         ItemBase postItem = new ItemBase();
         postItem.description = "post test";
         postItem.price = "";
-        postItem.sku = UUID.randomUUID().toString();
+        postItem.sku = randomUUID().toString();
 
         executeNegativePostTest(postItem);
     }
 
+    /**
+     * test POST for an empty sku
+     * @throws JsonProcessingException
+     */
     @Test()
     public void postEmptySkuTest() throws JsonProcessingException {
         ItemBase postItem = new ItemBase();
@@ -100,43 +136,73 @@ public class APITests {
         executeNegativePostTest(postItem);
     }
 
+    /**
+     * execute the GET API that returns all the sku data
+     * and run validation tests
+     * @return all sku items resulting from the API
+     * @throws JsonProcessingException
+     */
     private Item[] executeGetAllTest() throws JsonProcessingException {
         Response response = httpRequest.get("");
         int statusCode = response.getStatusCode();
-        Assert.assertEquals(statusCode, 200, "expect 200 result");
+        assertEquals(statusCode, 200, "expect 200 result");
         String body = response.getBody().asString();
         return objectMapper.readValue(body, Item[].class);
     }
 
-    private Item executeGetTest(Item imputItem) throws JsonProcessingException {
-        Response getResponse = httpRequest.get("/" + imputItem.sku);
+    /**
+     * run the GET API for a particular sku and validate the result
+     * @param inputItem
+     * @return Item object resulting from the GET-all API for a particular sku
+     * @throws JsonProcessingException
+     */
+    private Item executeGetTest(Item inputItem) throws JsonProcessingException {
+        Response getResponse = httpRequest.get("/" + inputItem.sku);
         int statusCode = getResponse.getStatusCode();
-        Assert.assertEquals(statusCode, 200, "expect 200 result");
+        assertEquals(statusCode, 200, "expect 200 result");
         String getResponseBody = getResponse.getBody().asString();
         SkuMetadata getResponseSku = objectMapper.readValue(getResponseBody, SkuMetadata.class);
-        Assert.assertEquals(getResponseSku.ResponseMetadata.HTTPStatusCode, statusCode, "inconsistent status code");
-        Assert.assertEquals(getResponseSku.ResponseMetadata.HTTPHeaders.content_type, "application/x-amz-json-1.0", "unexpected content-type");
-        Assert.assertTrue(getResponseSku.ResponseMetadata.RetryAttempts >= 0, "retry attempts is non-negative");
-        validateItem(getResponseSku.Item, imputItem);
+        assertEquals(getResponseSku.ResponseMetadata.HTTPStatusCode, statusCode, "inconsistent status code");
+        assertEquals(getResponseSku.ResponseMetadata.HTTPHeaders.content_type, "application/x-amz-json-1.0", "unexpected content-type");
+        assertTrue(getResponseSku.ResponseMetadata.RetryAttempts >= 0, "retry attempts is non-negative");
+        validateItem(getResponseSku.Item, inputItem);
 
         return getResponseSku.Item;
     }
 
-    private void executeGetTestWithTimeChecks(Item postResponseItem) throws JsonProcessingException {
-        Long timeBeforeGet = Instant.now().getEpochSecond();
-        Item getResponseItem = executeGetTest(postResponseItem);
-        Long timeAfterGet = Instant.now().getEpochSecond();
-        Long createdAt = Long.parseLong(getResponseItem.createdAt);
-        Assert.assertTrue(createdAt >= timeBeforeGet && createdAt <= timeAfterGet,
-                "test that 'createdAt' is in the right time interval");
+    private void executeGetTestWithTimeChecks(Item postResponseItem, Long minCreatedAt, Long maxCreatedAt) throws JsonProcessingException {
+        executeGetTestWithTimeChecks(postResponseItem, minCreatedAt,maxCreatedAt, minCreatedAt, maxCreatedAt);
     }
 
+    /**
+     * test the get functionality and also check that the timestamps are correct
+     * @param postResponseItem data from the POST response for the particular sku
+     * @throws JsonProcessingException
+     */
+    private void executeGetTestWithTimeChecks(Item postResponseItem, Long minCreatedAt, Long maxCreatedAt,
+                                              Long minUpdatedAt, Long maxUpdatedAt) throws JsonProcessingException {
+        Item getResponseItem = executeGetTest(postResponseItem);
+        Long createdAt = Long.parseLong(getResponseItem.createdAt);
+        Long updateAt = Long.parseLong(getResponseItem.updatedAt);
+        assertTrue(createdAt >= minCreatedAt && createdAt <= maxCreatedAt,
+                "test that 'createdAt' is in the right time interval");
+        assertTrue(updateAt >= minUpdatedAt && updateAt <= maxUpdatedAt,
+                "test that 'updateAt' is in the right time interval");
+
+    }
+
+    /**
+     * execute the POST API and validate the result
+     * @param postItem data for the POST body
+     * @return output of the POST
+     * @throws JsonProcessingException
+     */
     private Item executePostTest(ItemBase postItem) throws JsonProcessingException {
         httpRequest.header("Content-Type", "application/json");
         httpRequest.body(objectMapper.writeValueAsString(postItem));
         Response response = httpRequest.post("");
         int statusCode = response.getStatusCode();
-        Assert.assertEquals(statusCode, 200, "expect 200 result");
+        assertEquals(statusCode, 200, "expect 200 result");
         String responseBody = response.getBody().asString();
         Item postResponseItem = objectMapper.readValue(responseBody, Item.class);
         validateItemBase(postResponseItem, postItem);
@@ -144,38 +210,62 @@ public class APITests {
         return postResponseItem;
     }
 
+    /**
+     * execute the DELETE API and validate the result
+     * @param postItem output of the DELETE
+     * @throws JsonProcessingException
+     */
     private void executeDeleteTest(ItemBase postItem) throws JsonProcessingException {
         httpRequest.header("Content-Type", "application/json");
         Response deleteResponse = httpRequest.delete("/" + postItem.sku.toString());
         int statusCode = deleteResponse.getStatusCode();
-        Assert.assertEquals(statusCode, 200, "expect 200 result");
+        assertEquals(statusCode, 200, "expect 200 result");
         String deleteResponseBody = deleteResponse.getBody().asString();
-        Assert.assertEquals(deleteResponseBody, "", "expect empty response body");
+        assertEquals(deleteResponseBody, "", "expect empty response body");
     }
 
+    /**
+     * test the POST API where the sku doesn't exist
+     * @param sku sku ID
+     */
     private void executeGetUnknownItemTest(String sku) {
         Response getResponse = httpRequest.get("/" + sku);
         int statusCode = getResponse.getStatusCode();
-        Assert.assertEquals(statusCode, 404, "expect 404 result for a sku that doesn't exist");
+        assertEquals(statusCode, 404, "expect 404 result for a sku that doesn't exist");
     }
 
+    /**
+     * POST API test where certain fields are empty
+     * @param postItem data for the POST body
+     * @throws JsonProcessingException
+     */
     private void executeNegativePostTest(ItemBase postItem) throws JsonProcessingException {
         httpRequest.header("Content-Type", "application/json");
         httpRequest.body(objectMapper.writeValueAsString(postItem));
         Response response = httpRequest.post("");
         int statusCode = response.getStatusCode();
-        Assert.assertEquals(statusCode, 400, "expect 400 for a malformed POST body");
+        assertEquals(statusCode, 400, "expect 400 for a malformed POST body");
     }
 
+    /**
+     * compare values between Item objects
+     * @param actualItem
+     * @param expectedItem
+     */
     private void validateItem(Item actualItem, Item expectedItem) {
-        Assert.assertEquals(actualItem.createdAt, expectedItem.createdAt, "test for 'createdAt' consistency");
-        Assert.assertEquals(actualItem.updatedAt, expectedItem.updatedAt, "test for 'updatedAt' consistency");
+        assertEquals(actualItem.createdAt, expectedItem.createdAt, "test for 'createdAt' consistency");
+        assertEquals(actualItem.updatedAt, expectedItem.updatedAt, "test for 'updatedAt' consistency");
         validateItemBase(actualItem, expectedItem);
     }
 
+    /**
+     * compare values ItemBase objects
+     * @param actualItem
+     * @param expectedItem
+     */
     private void validateItemBase(ItemBase actualItem, ItemBase expectedItem) {
-        Assert.assertEquals(actualItem.description, expectedItem.description, "test for 'description' consistency");
-        Assert.assertEquals(actualItem.price, expectedItem.price, "test for 'price' consistency");
-        Assert.assertEquals(actualItem.sku, expectedItem.sku, "test for 'sku' consistency");
+        assertEquals(actualItem.description, expectedItem.description, "test for 'description' consistency");
+        assertEquals(actualItem.price, expectedItem.price, "test for 'price' consistency");
+        assertEquals(actualItem.sku, expectedItem.sku, "test for 'sku' consistency");
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # AIM Coding Assessment
 
 The following is code for a set of tests to verify the results
-of a [SKU API](https://coderbyte.com/question/api-test-automation-ieh70kchsk).
+of a [SKU API](https://coderbyte.com/question/api-test-automation-ieh70kchsk)
+with the intention of exercising its basic functionality as
+a web service by invoking positive and negative test cases.
 
 ## Setup and Running
 
@@ -24,20 +26,26 @@ The tests take advantage of the
 [Maven Build Lifecycle](https://maven.apache.org/guides/introduction/introduction-to-the-lifecycle.html)
 and use 
 [Rest Assured](https://rest-assured.io/) 
-as the REST Api test framework.
+as the REST Api test framework.  The test framework is 
+[TestNg](https://testng.org/doc/index.html) in Java.
+The tests look for seemingly correct results and consistency
+between API calls.  JSON objects are handled using
+[FasterXML/jackson](https://github.com/FasterXML/jackson).
 
 In addition, the tests take into consideration that the
 stored data accessible through the API's may be needed
-by other users, and seek to avoid modifying
+by other users, and seeks to avoid modifying
 or deleting the data already there.  The tests involving
-POST and DELETE therefore only operate on data created by
-the user.  For this reason, the test suite contains primarily
-end-to-end scenarios rather than tests that target
-one API alone.  In a real world scenario, it would be
-better to consult with teammates while making such a choice.
+POST and DELETE, therefore, only operate on data created 
+internally within the tests.  
+For this reason, the positive test cases are primarily
+end-to-end rather than tests that target one API at a time.  
 
-The tests look for seemingly correct results and consistency
-between API calls.
+In a real world scenario, it would be
+better to consult with teammates while making such a choice.
+A collaborative effort could set up a test backend where
+data is disposable, or provide hooks to pre-seed the data,
+or other provisions to avoid problematic issues.
 
 ## Test Scenarios
 
@@ -47,12 +55,16 @@ The following test scenarios are covered
 checking for consistency between the two
 2. validate the following end-to-end scenario
    1. POST
-   2. GET to check that the POST arrived
+   2. GET to check that the POST populated the data
    3. POST to update the values in step 1
    4. GET to check the update
    5. DELETE the sku from step 1
-   6. GET to check that the sku no longer shows up
-3. negative tests invalid POST body where certain fields
-are empty
+   6. GET to check that the deleted sku no longer shows up
+3. negative tests for invalid POST bodies where 
+certain fields are empty
 4. negative test to GET an unknown sku
 5. negative test to DELETE an unknown sku
+
+It is not guaranteed that all the tests pass, but
+the tests attempt to convey reasons for failures when
+the APIs behave in unexpected ways.
